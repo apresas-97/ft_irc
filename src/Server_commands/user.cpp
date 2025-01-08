ca46d7591ffec809a79a85b59665d34d33678334
apresas- <albertprpu97@gmail.com>
2025-01-08 17:46:02 +0100

    improved cmdUser

--- src/Server_commands/user.cpp
+++ src/Server_commands/user.cpp
@@ -1,51 +1,119 @@
 #include "Server.hpp"
 
+
+// NOTE: The IRC protocol doesn't specify syntax rules for the username and realname
+static bool	irc_isValidUsername( const std::string & username )
+{
+	if (username.size() < 1 || username.size() > 15)
+		return false;
+	for (size_t i = 0; i < username.size(); i++)
+	{
+		// Only allow printable ASCII characters, except spaces
+		if (username[i] < 33 || username[i] > 126)
+			return false;
+	}
+	return true;
+}
+
+// NOTE: The IRC protocol doesn't specify syntax rules for the username and realname
+static bool	irc_isValidRealname( const std::string & realname )
+{
+	if (realname.size() < 1 || realname.size() > 50)
+		return false;
+	for (size_t i = 0; i < realname.size(); i++)
+	{
+		// Only allow printable ASCII characters
+		if (realname[i] < 32 || realname[i] > 126)
+			return false;
+	}
+	return true;
+}
+
 /*
 Command: USER
+Parameters: <username> <unused> <unused> <realname>
+USER command is used at the beginning of connection to specify the identity of the user.
+There are two legacy parameters which are unused nowadays.
+
+// OLD DEFINITION:
 Parameters: <username> <mode> <unused> <realname>
 USER command is used at the beginning of connection to specify the identity of the user.
 <mode> should be numeric, and it's a bitmask for modes 'w'(bit 2) and 'i'(bit 3). The rest of bits
 are irrelevant.
 <realname> may contain space characters.
-
-New info:
-Apparently, nowadays, the <mode> parameter is ignored completely by most servers.
 */
 std::vector<t_message>	Server::cmdUser( t_message & message ) 
 {
 	std::cout << "USER command called..." << std::endl;
+	Client * client = this->_current_client;
 	std::vector<t_message> replies;
+
+	// Check if the client is already registered, this takes precedence over everything else
+	if (client->isRegistered() == true)
+	{
+		replies.push_back(createReply(ERR_ALREADYREGISTRED, ERR_ALREADYREGISTRED_STR));
+		return replies;
+	}
+
 	if (message.params.size() < 4) 
 	{
 		replies.push_back(createReply(ERR_NEEDMOREPARAMS, ERR_NEEDMOREPARAMS_STR, this->_current_client->getNickname()));
 		return replies;
 	}
+
 	if (this->_current_client->getNickname().empty() == true) 
 	{
-		// Idk what to do here, the nickname should have been set already
-		// If not, the registration can't continue
-		// I need to figure this out
-		return replies; // ???
-	}
-	Client * client = this->_current_client;
-	if (client->isRegistered() == true) 
-	{
-		replies.push_back(createReply(ERR_ALREADYREGISTRED, ERR_ALREADYREGISTRED_STR));
+		/* I haven't been able to get a definitive answer on the order of the NICK and USER commands
+		But from what I've gathered, it really seems like the NICK command should come first
+		For this reason, if the USER message is sent first, the server will silently ignore it */
 		return replies;
 	}
-	// apresas-: TODO: Check that the username and realname are not too long
-	// But if they are, I don't know how to handle it yet because the protocol doesn't
-	// specify what to do in that case
-	// It doesn't really even specify a limit for username or realname length
-	// But we should set some reasonable limit or we could be vulnerable to
-	// all sort of buffer overflow attacks, etc.
-	client->setUsername(message.params[0]);
-	client->setHostname(message.params[1]);
-	client->setRealname(message.params[3]);
 
+	// NOTICE reply for invalid username or realname syntax
+	t_message notice_reply;
+	notice_reply.prefix = this->getName();
+	notice_reply.command = "NOTICE";
+	notice_reply.target_client_fds.insert(client->getSocket());
+
+	if (irc_isValidUsername(message.params[0]) == false)
+	{
+		notice_reply.params.push_back(client->getNickname() + " :Invalid username syntax (only printable ASCII and must be between 1 and 15 characters)");
+		replies.push_back(notice_reply);
+		return replies;
+	}
+	if (irc_isValidRealname(message.params[3]) == false)
+	{
+		notice_reply.params.push_back(client->getNickname() + " :Invalid realname syntax (only printable ASCII and must be between 1 and 50 characters)");
+		replies.push_back(notice_reply);
+		return replies;
+	}
+
+	client->setUsername(message.params[0]);
+	client->setRealname(message.params[3]);
+	// TODO: Actually get the hostname, maybe here or maybe somewhere else
+	/*
+		- We need to have access to the client's IP address with its sockaddr_in struct
+			std::string ip_address = inet_ntoa(client_addr.sin_addr);
+
+		Then we can get the hostname like this:
+
+		struct in_addr	addr;
+		struct hostent	*host_entry;
+
+		addr.s_addr = inet_addr(ip_address.c_str());
+
+		// We performe reverse DNS lookup
+		host = gethostbyname(inet_ntoa(addr));
+		if (host == NULL)
+			// No hostname found, we just use the IP address as the hostname
+		else
+			hostname = host->h_name;
+
+		I'm writing this here instead of implementing it now because I don't want to add more
+		changes to the repository at once until everything is merged and consolidated in the main branch.
+	*/
 	client->setRegistered(true);
 
-	// TODO:
 	// Prepare the welcome message reply
 	replies.push_back(createReply(RPL_WELCOME, RPL_WELCOME_STR, client->getUserIdentifier()));
 	std::vector<std::string> yourhost_params;
@@ -56,8 +124,9 @@ std::vector<t_message>	Server::cmdUser( t_message & message )
 	std::vector<std::string> myinfo_params;
 	myinfo_params.push_back(this->getName());
 	myinfo_params.push_back(this->getVersion());
-	myinfo_params.push_back(USER_MODES); // Here will go all of the available user modes
-	myinfo_params.push_back(CHANNEL_MODES); // Here will go all of the available channel modes
+	// These are TODO because I don't know if those are the definitive modes or if this is how we'll handle it
+	myinfo_params.push_back(USER_MODES); // TODO: Here will go all of the available user modes
+	myinfo_params.push_back(CHANNEL_MODES); // TODO: Here will go all of the available channel modes
 	replies.push_back(createReply(RPL_MYINFO, RPL_MYINFO_STR, myinfo_params));
 
 	return replies;
