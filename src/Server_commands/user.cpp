6d8feae8549bad17863328a7ce1fcbd8c8cee5ae
apresas- <albertprpu97@gmail.com>
2025-01-10 14:48:59 +0100

    cmdUser: Added a check for isAuthorised of the client to make sure its not set as registered in that case and it does not return the WELCOME replies to it

--- src/Server_commands/user.cpp
+++ src/Server_commands/user.cpp
@@ -117,6 +117,10 @@ std::vector<t_message>	Server::cmdUser( t_message & message )
 	client->setUsername(message.params[0]);
 	client->setRealname(message.params[3]);
 
+	// If the client is not authorised, then of course, it won't be registered and won't get the WELCOME replies
+	if (client->isAuthorised() == false)
+		return replies;
+
 	client->setRegistered(true);
 
 	return createWelcomeReplies(client);
