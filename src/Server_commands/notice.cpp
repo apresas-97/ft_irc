49deb212fdeaabe730bd23dea48dc9a63688754c
apresas- <albertprpu97@gmail.com>
2025-01-10 17:44:14 +0100

    noticeCmd: Possible implementation, I don't check it as DONE yet because there might be more to it

--- src/Server_commands/notice.cpp
+++ src/Server_commands/notice.cpp
@@ -10,10 +10,15 @@ in response to a NOTICE message.
 
 It's purpose is avoiding loops between clients automatically sending
 eachother automatic replies.
-
 */
 std::vector<t_message>	Server::cmdNotice( t_message & message )
 {
-	// TODO
-	// Until PRIVMSG is fully implemented I don't see a point in implementing NOTICE
+	// Possible implementation? I don't know if it will be so simple
+	std::vector<t_message> replies = cmdPrivMsg(message);
+	for (std::vector<t_message>::iterator it = replies.begin(); it != replies.end(); ++it)
+	{
+		if (it->command == "PRIVMSG")
+			it->command = "NOTICE";
+	}
+	return replies;
 }
