6ae3594d6538d0bc9a70d091f37908b891f3e242
ffornesp <ffornesp@gmail.com>
2025-01-27 08:23:39 +0100

    Server_commands/who.cpp : Implemented basics

--- src/Server_commands/who.cpp
+++ src/Server_commands/who.cpp
@@ -36,6 +36,41 @@
 
 std::vector<t_message> Server::cmdWho( t_message & message )
 {
-	//	....	
+	std::vector<t_message>	replies;
+	std::string				name;
+
+	if (message.params.size() > 0)
+		name = message.params[0];
+
+	bool	is_wildcard = (name.empty() || name == "0" || name == "*");
+	bool	is_channel = (!name.empty() && (name[0] == '#' || name[0] == '&'));
+
+	if (is_wildcard || name.find('*') != std::string::npos)
+	{
+		std::cout << "It is wildcard" << std::endl;
+	}
+	else if (is_channel)
+	{
+		std::cout << "It is channel" << std::endl;
+		Channel *	channel = this->findChannel(name);
+
+		if (channel && channel->isUserInChannel(this->_current_client->getUsername()))
+		{
+			std::map<std::string, Client *>	users = channel->getTrueUsers();
+			for (std::map<std::string, Client *>::iterator it = users.begin(); it != users.end(); ++it)
+			{
+				if (!it->second->getMode('i')) // Add user to who list...
+				{
+					std::cout << "Adding user: " << it->second->getNickname() << std::endl;
+				}
+			}
+		}
+		else // Channel not found or User not in channel
+		{
+			std::cout << "Unable to process who command" << std::endl;
+		}
+	}
+	// Send end of who
+	return replies;
 }
 
