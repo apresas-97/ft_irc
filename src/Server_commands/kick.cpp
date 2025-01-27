c4b5a1a23bd0d1759aec04980b26186f1cd2a8c3
ffornesp <ffornesp@gmail.com>
2025-01-27 19:06:51 +0100

    Server_commands/kick.cpp : Fixed kick

--- src/Server_commands/kick.cpp
+++ src/Server_commands/kick.cpp
@@ -1,159 +1,147 @@
 #include "Server.hpp"
 /*
 
-    PSEUDO
+	PSEUDO
 
-    1. Access the appropriate client (Executor)
-        - Get their nickname
-    2. Prepare the kick message
+	1. Access the appropriate client (Executor)
+		- Get their nickname
+	2. Prepare the kick message
 
-    3. Error handling
-        - Not enough parameters
-        - Channel does not exist
-        - Client is not in the channel
-        - Client is not an operator: cannot kick anyone
-    4. If there are 4 or more parameters
-        - Assemble the kick message
+	3. Error handling
+		- Not enough parameters
+		- Channel does not exist
+		- Client is not in the channel
+		- Client is not an operator: cannot kick anyone
+	4. If there are 4 or more parameters
+		- Assemble the kick message
 
-    5. Handle targets
-        - Errors
-            - Target nickname does not exist
-            - Target to be kicked is not in the channel
-        
-        - Send message
-        - Remove the client from all lists
+	5. Handle targets
+		- Errors
+			- Target nickname does not exist
+			- Target to be kicked is not in the channel
+		
+		- Send message
+		- Remove the client from all lists
 
-    6. Delete the channel if it has no more users
+	6. Delete the channel if it has no more users
 
-    Format
+	Format
 
-    KICK <channel> <user> [<reason>]
-    KICK <channel> <user1,user2,user3> [<reason>] 
+	KICK <channel> <user> [<reason>]
+	KICK <channel> <user1,user2,user3> [<reason>] 
 
 */
 
 static std::vector<std::string> parseMessage(const std::string &message, char delimiter) 
 {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream tokenStream(message);
+	std::vector<std::string> tokens;
+	std::string token;
+	std::istringstream tokenStream(message);
 
-    while (std::getline(tokenStream, token, delimiter))
-        tokens.push_back(token);
+	while (std::getline(tokenStream, token, delimiter))
+		tokens.push_back(token);
 
-    return tokens;
+	return tokens;
 }
 
 
 std::vector<t_message>	Server::cmdKick( t_message & message )
 {
 	std::cout << "KICK command called..." << std::endl;
-    std::vector<t_message> replies;
+	std::vector<t_message> replies;
 
-    Client *client = findClient(message.sender_client_fd);
-    std::string nick = client->getNickname();
-    std::string mainMsg = "";
+	Client * client = findClient(message.sender_client_fd);
+	std::string nick = client->getNickname();
+	std::string mainMsg = "";
 
-    if (client->isRegistered() == false)
+	if (client->isRegistered() == false)
 	{
 		replies.push_back(createReply(ERR_NOTREGISTERED, ERR_NOTREGISTERED_STR));
 		return replies;
 	}
 
-    if (message.params.size() < 2)
-    {
+	if (message.params.size() < 2)
+	{
 		std::vector<std::string>	params;
 		params.push_back(nick);
 		params.push_back("KICK");
-        replies.push_back(createReply(ERR_NEEDMOREPARAMS, ERR_NEEDMOREPARAMS_STR, params));
-        return replies;
-    }
+		replies.push_back(createReply(ERR_NEEDMOREPARAMS, ERR_NEEDMOREPARAMS_STR, params));
+		return replies;
+	}
 
-    std::string channel = message.params[1];
-    if (!findChannel(channel))
-    {
-        replies.push_back(createReply(ERR_NOSUCHCHANNEL, ERR_NOSUCHCHANNEL_STR, channel));
-        return replies;
-    }
-    if (!findChannel(channel)->isUserInChannel(nick))
-    {
-        replies.push_back(createReply(ERR_NOTONCHANNEL, ERR_NOTONCHANNEL_STR, channel));
-        return replies;
-    }
-    if (!findChannel(channel)->isUserOperator(nick)){
-        replies.push_back(createReply(ERR_CHANOPRIVSNEEDED, ERR_CHANOPRIVSNEEDED_STR, channel));
-        return replies;
-    }
+	std::vector<std::string> channel_targets = parseMessage(message.params[0], ',');
+	for (std::vector<std::string>::iterator it = channel_targets.begin(); it != channel_targets.end(); ++it)
+	{
+		Channel	*	channel = findChannel(*it);
 
-    if (message.params.size() >= 4)
-    {
-        for (size_t i = 3; i < message.params.size(); i++)
-        {
-            mainMsg += message.params[i];
-            if (i < message.params.size() - 1)
-                mainMsg += " ";
-        }
-    }
-    else
-    {
-        mainMsg = nick + " has kicked user from channel"; // ??
-    }
+		if (!channel)
+			replies.push_back(createReply(ERR_NOSUCHCHANNEL, ERR_NOSUCHCHANNEL_STR, *it));
+		else if (!channel->isUserInChannel(nick)) 
+			replies.push_back(createReply(ERR_NOTONCHANNEL, ERR_NOTONCHANNEL_STR, *it));
+		else if (!channel->isUserOperator(nick))
+			replies.push_back(createReply(ERR_CHANOPRIVSNEEDED, ERR_CHANOPRIVSNEEDED_STR, *it));
+	}
 
-    std::vector<std::string> targets = parseMessage(message.params[2], ',');
-    for (size_t i = 0; i < targets.size(); i++)
-    {
-        if (!findClient(targets[i]))
-        {
-            replies.push_back(createReply(ERR_NOSUCHNICK, ERR_NOSUCHNICK_STR, targets[i]));
-            continue;
-        }
-        if (!findChannel(channel)->isUserInChannel(targets[i]))
-        {
-			std::vector<std::string>	params;
-			params.push_back(targets[i]);
-			params.push_back(channel);
-            replies.push_back(createReply(ERR_USERNOTINCHANNEL, ERR_USERNOTINCHANNEL_STR, params));
-            continue;
-        }
+	if (message.params.size() >= 4)
+	{
+		for (size_t i = 3; i < message.params.size(); i++)
+		{
+			mainMsg += message.params[i];
+			if (i < message.params.size() - 1)
+				mainMsg += " ";
+		}
+	}
+	else
+	{
+		mainMsg = nick + " has kicked user from channel"; // ??
+	}
 
-        t_message kickMessage;
-        kickMessage.prefix = ":" + client->getUserPrefix();
-        kickMessage.command = "KICK";
-        kickMessage.params.push_back(channel);
-        kickMessage.params.push_back(targets[i]);
-        kickMessage.params.push_back(mainMsg);
-        kickMessage.sender_client_fd = client->getSocket();
-		kickMessage.target_client_fds.insert(message.sender_client_fd);
-        addChannelToReply(kickMessage, findChannel(channel));
+	std::vector<std::string> targets = parseMessage(message.params[1], ',');
+	for (size_t i = 0; i < targets.size(); i++)
+	{
+		if (!findClient(targets[i]))
+		{
+			replies.push_back(createReply(ERR_NOSUCHNICK, ERR_NOSUCHNICK_STR, targets[i]));
+			continue;
+		}
+		for (std::vector<std::string>::iterator it = channel_targets.begin(); it != channel_targets.end(); ++it)
+		{
+			Channel	*	channel = findChannel(*it);
 
-        findChannel(channel)->kickUser(targets[i]);
-        findChannel(channel)->uninviteUser(targets[i]);
-        removeClient(findClient(targets[i])->getSocket());
-    }
+			if (!channel)
+				continue;
+			if (!channel->isUserOperator(nick))
+				continue;
+			if (!channel->isUserInChannel(targets[i]))
+			{
+				std::vector<std::string>	params;
+				params.push_back(targets[i]);
+				params.push_back(*it);
+				replies.push_back(createReply(ERR_USERNOTINCHANNEL, ERR_USERNOTINCHANNEL_STR, params));
+				continue;
+			}
 
-    if (findChannel(channel)->isEmpty())
-    {
-        replies.push_back(createReply(RPL_CHANNELREMOVED, RPL_CHANNELREMOVED_STR, channel));
-		removeChannel(channel);
-    }
+			t_message kickMessage;
+			kickMessage.prefix = ":" + client->getUserPrefix();
+			kickMessage.command = "KICK";
+			kickMessage.params.push_back(*it);
+			kickMessage.params.push_back(targets[i]);
+			kickMessage.params.push_back(mainMsg);
+			kickMessage.sender_client_fd = client->getSocket();
+			kickMessage.target_client_fds.insert(findClient(targets[i])->getSocket());
+			addChannelToReply(kickMessage, channel);
+			replies.push_back(kickMessage);
 
-    // Send updated user list to the channel
-    std::vector<std::string> clientList = findChannel(channel)->getUsers();
-    if (!clientList.empty())
-    {
-        std::vector<std::string> paramsName = clientList;
-        paramsName.push_back(channel);
-        t_message nameReply = createReply(RPL_NAMREPLY, RPL_NAMREPLY_STR, paramsName);
-        addChannelToReply(nameReply, findChannel(channel));
-        replies.push_back(nameReply);
+			channel->kickUser(targets[i]);
+			channel->uninviteUser(targets[i]);
+			// TODO client remove channels from channels vector
+			if (channel->isEmpty())
+			{
+				replies.push_back(createReply(RPL_CHANNELREMOVED, RPL_CHANNELREMOVED_STR, *it));
+				removeChannel(*it);
+			}
+		}
+	}
 
-        std::vector<std::string> paramsEnd = clientList;
-        paramsEnd.push_back(client->getNickname());
-        paramsEnd.push_back(channel);
-        t_message endOfNames = createReply(RPL_ENDOFNAMES, RPL_ENDOFNAMES_STR, paramsEnd);
-        addChannelToReply(endOfNames, findChannel(channel));
-        replies.push_back(endOfNames);
-    }
-
-    return replies;
+	return replies;
 }
